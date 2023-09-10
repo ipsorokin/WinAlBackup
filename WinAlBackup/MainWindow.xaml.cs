d2d74010f8d2574f2833968efc17653b217fbc20
dmx07 <dmx077@yandex.ru>
2023-09-10 22:43:46 +0300

    Добавьте файлы проекта.

---
+++ WinAlBackup/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using WinAlBackup.ViewModels;
+
+namespace WinAlBackup
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F5)
+            {
+                ((MainViewModel)DataContext).ReloadBackupFiles();
+            }
+        }
+    }
+}
